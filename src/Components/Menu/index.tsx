a01ba8f3a93f22b5eb950605487d15bc04c7126a
Lucas Alfeu <103508524+LucasAlfeu@users.noreply.github.com>
2022-11-26 11:23:44 -0300

    corrigindo ortografia

--- src/Components/Menu/index.tsx
+++ src/Components/Menu/index.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export default function Menu() {
 	return (
 		<header className={styles.cabecalho}>
-			<h1 className={styles.cabecalho__titulo}>PORTIFÓLIO</h1>
+			<h1 className={styles.cabecalho__titulo}>PORTFÓLIO</h1>
 			<ul className={styles.cabecalho__lista}>
 				<li><Link to={'/'} className={styles.cabecalho__itemPrimeiro}>Inicio</Link></li>
 				<li><Link to={'/projetos'} className={styles.cabecalho__item}>Projetos</Link></li>
