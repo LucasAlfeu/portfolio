9e69d38618c0e21c2f1845e34e4cf9cf8debee35
Lucas Alfeu <103508524+LucasAlfeu@users.noreply.github.com>
2022-11-26 11:28:08 -0300

    ortografia

--- src/Pages/Inicio/index.tsx
+++ src/Pages/Inicio/index.tsx
@@ -6,7 +6,7 @@ export default function Inicio() {
 	return (
 		<div>
 			<main className={styles.principal}>
-				<p className={styles.principal__texto}>Olá, meu nome é Lucas. Sou <strong>desenvolvedor Front-End</strong> e decidi criar esse projeto para reunir em um só lugar todos os meus outros projetos de protfólio, além de mais algumas informações pessoais para contado, um pouco sobre mim e também meu currículo. Eu comecei a perceber que faltava organizar tudo num só lugar, principalmente meus projetos. Então seja bem vindo.</p>
+				<p className={styles.principal__texto}>Olá, meu nome é Lucas. Sou <strong>desenvolvedor Front-End</strong> e decidi criar esse projeto para reunir em um só lugar todos os meus outros projetos de protfólio, além de mais algumas informações pessoais para contado, um pouco sobre mim e também meu currículo. Eu comecei a perceber que faltava organizar tudo num só lugar. Então seja bem vindo.</p>
 				<Foto className={styles.principal__foto} />
 				<p className={styles.principal__informacao}>Lucas Alfeu da Silva Oliveira - Volta Redonda, Rio de Janeiro</p>
 			</main>
