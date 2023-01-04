0f423aa363ba491d3d44b8878ece34871231cb99
Lucas Alfeu <103508524+LucasAlfeu@users.noreply.github.com>
2023-01-04 07:37:19 -0300

    Trocando minha foto de apresentação

--- src/Pages/Inicio/index.tsx
+++ src/Pages/Inicio/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from './Inicio.module.scss';
-import { ReactComponent as Foto } from 'assets/foto.svg';
+// import { ReactComponent as Foto } from 'assets/foto.svg';
+import fotoPerfil from 'assets/fotoPerfil.jpg';
 
 export default function Inicio() {
 	return (
 		<div>
 			<main className={styles.principal}>
 				<p className={styles.principal__texto}>Olá, meu nome é Lucas. Sou <strong>desenvolvedor Front-End</strong> e decidi criar esse projeto para reunir em um só lugar todos os meus outros projetos de protfólio, além de mais algumas informações pessoais para contado, um pouco sobre mim e também meu currículo. Eu comecei a perceber que faltava organizar tudo num só lugar. Então seja bem vindo.</p>
-				<Foto className={styles.principal__foto} />
+				{/* <Foto className={styles.principal__foto} /> */}
+				<img  className={styles.principal__foto} src={fotoPerfil} alt="Foto de perfil" />
 				<p className={styles.principal__informacao}>Lucas Alfeu da Silva Oliveira - Volta Redonda, Rio de Janeiro</p>
 			</main>
 		</div>
